19bb2d0dce5bb769246549ca9ac2d7826020758e
Manuel Fideles <manuelfideles77@gmail.com>
2022-07-08 14:38:10 +0100

    added tiling, non-responsive yet

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import GalleryItem from '../GalleryItem';
 import Spinner from '../Spinner';
+import Masonry from 'react-masonry-css';
 
-import { gallery, load } from './Gallery.module.css'
+import { gallery, load, _grid, _column } from './Gallery.module.css'
 
 export default function Gallery({ posts }) {
 
-    function displayGallery(arr) {
-        return arr.map(post => {
-            if (post['embedded']['wp:featuredmedia'] !== undefined) {
-                return <GalleryItem key={post.id}
-                    title={post.title}
-                    link={post.link}
-                    thumbnail={post['embedded']['wp:featuredmedia'][0]}
-                />
-            }
-        })
-    }
+    let postArray = posts.map(post => {
+        if (post['embedded']['wp:featuredmedia'] !== undefined) {
+            return <GalleryItem key={post.id}
+                title={post.title}
+                link={post.link}
+                thumbnail={post['embedded']['wp:featuredmedia'][0]}
+            />
+        }
+    })
 
     if (!posts) return <Spinner />;
     return (
         <>
-            <section className={gallery}>
-                {displayGallery(posts)}
-            </section>
+            <Masonry
+                breakpointCols={4}
+                className={_grid}
+                columnClassName={_column}
+            >{postArray}</Masonry>
             <div className={load}>
                 <span>LOAD MORE</span>
             </div>
--- components/GalleryItem/GalleryItem.js
+++ components/GalleryItem/GalleryItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
 
-import { post_title } from './GalleryItem.module.css';
+import { item, post_title, post_image } from './GalleryItem.module.css';
 
 export default function GalleryItem({ title, link, thumbnail }) {
     /* 
@@ -9,15 +8,16 @@ export default function GalleryItem({ title, link, thumbnail }) {
      * projects = 2x2
      * news = 2x1
      */
-    console.log(link, thumbnail);
     return (
-        <a href={link}>
-            <Image
+        <div className={item}>
+            <img
                 src={thumbnail['media_details'] ? thumbnail['media_details'].sizes.full['source_url'] : '/images/placeholder.png'}
-                width={180} height={120}
                 alt={thumbnail["alt_text"]}
+                className={post_image}
             />
-            <span className={post_title}>{title}</span>
-        </a>
+            <span className={post_title}>
+                <a href={link}>{title}</a>
+            </span>
+        </div>
     )
 }
