3f80e86680920b6434086eb2b62841256efaab7b
Manuel Fideles <manuelfideles77@gmail.com>
2022-09-08 15:18:45 +0100

    Replaced Logo by its .jpg Version

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,15 +1,17 @@
+
 import styles from './GalleryItem.module.scss';
 
 export default function GalleryItem({ title, link, thumbnail, term }) {
   const renderThumbnail = () => {
     return ![undefined, null].includes(thumbnail)
       ? thumbnail
-      : '/assets/images/placeholder.png'
+      : '/assets/images/placeholder.jpg'
   }
   return (
     <div className={styles.itemCard}>
       <img
         src={renderThumbnail()}
+        loading='lazy'
         alt='post thumbnail'
         height={term === 'projects' ? '300px' : '150px'}
       />
